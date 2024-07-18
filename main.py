e09a16d29641c320af6c7ce686acfe2d07fe8ee3
denilsonsoares <166926902+denilsonsoares@users.noreply.github.com>
2024-07-18 14:52:46 -0300

    ler arquivos de imagens

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import openai
 import pandas as pd
 from PyPDF2 import PdfReader
 from docx import Document
+from PIL import Image
+import google.generativeai as genai
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
-# Função para ler a chave da API do arquivo .txt
-def get_api_key():
-    with open('api_key.txt', 'r') as file:
+# Função para ler a chave da API do Gemini e do OpenAI do arquivo .txt
+def get_api_key(filename):
+    with open(filename, 'r') as file:
         return file.read().strip()
 
+# Configuração da API do Gemini
+genai.configure(api_key=get_api_key('gemini_api_key.txt'))
+
 # Configuração da API do OpenAI
-api_key = get_api_key()
+openai_api_key = get_api_key('openai_api_key.txt')
 
 # Função para extrair texto de PDF
 def extract_text_from_pdf(file_path):
@@ -31,7 +36,14 @@ def extract_text_from_docx(file_path):
         full_text.append(para.text)
     return '\n'.join(full_text)
 
-# Função para extrair texto de arquivos PDF, TXT e DOCX
+# Função para extrair texto de imagens usando a API do Gemini
+def extract_text_from_image(file_path):
+    img = Image.open(file_path)
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content(img)
+    return response.text
+
+# Função para extrair texto de arquivos PDF, TXT, DOCX e imagens
 def extract_text_from_file(file_path):
     if file_path.endswith('.pdf'):
         return extract_text_from_pdf(file_path)
@@ -40,6 +52,8 @@ def extract_text_from_file(file_path):
             return file.read()
     elif file_path.endswith('.docx'):
         return extract_text_from_docx(file_path)
+    elif file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
+        return extract_text_from_image(file_path)
     else:
         return ''
 
@@ -53,7 +67,7 @@ def extract_job_info(text):
         Informações extraídas:
         """
     prompt = PromptTemplate(input_variables=["text"], template=template)
-    llm = ChatOpenAI(model_name="gpt-4", openai_api_key=api_key)
+    llm = ChatOpenAI(model_name="gpt-4", openai_api_key=openai_api_key)
     chain = LLMChain(prompt=prompt, llm=llm)
     result = chain.run(text)
     return result.strip()
@@ -65,7 +79,7 @@ def main():
 
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
-        if file_path.endswith('.pdf') or file_path.endswith('.txt') or file_path.endswith('.docx'):
+        if file_path.endswith(('.pdf', '.txt', '.docx', '.png', '.jpg', '.jpeg')):
             text = extract_text_from_file(file_path)
             if text:
                 job_info = extract_job_info(text)
