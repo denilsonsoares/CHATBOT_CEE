537de6bde05e784d5fa57ae1ace4da0339be6f1d
denilsonsoares <166926902+denilsonsoares@users.noreply.github.com>
2024-07-25 21:14:13 -0300

    Create token_counter.py

---
+++ token_counter.py
@@ -0,0 +1,35 @@
+import os
+from PyPDF2 import PdfReader
+from transformers import GPT2Tokenizer
+
+# Função para ler texto de um arquivo PDF
+def extract_text_from_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+# Função para contar tokens em um texto usando o tokenizador GPT-2
+def count_tokens(text):
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+# Função principal para contar tokens em todos os arquivos PDF em uma pasta
+def main():
+    folder_path = './VAGAS'
+    pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
+    total_tokens = 0
+
+    for file_name in pdf_files:
+        file_path = os.path.join(folder_path, file_name)
+        text = extract_text_from_pdf(file_path)
+        num_tokens = count_tokens(text)
+        #print(f'O arquivo {file_name} possui {num_tokens} tokens.')
+        total_tokens += num_tokens
+
+    print(f'Total de tokens em todos os arquivos PDF: {total_tokens}')
+
+if __name__ == '__main__':
+    main()
